f654b74f06dc82f032ee8f4575c2738951e765d8
unknown <harriett.steed@gmail.com>
2023-03-15 23:43:51 -0600

    added js content

---
+++ Assets/script.js
@@ -0,0 +1,61 @@
+
+var buttonA = document.getElementById("")
+
+
+
+
+
+var Questions = [{
+    question:"Keydown is when...?";
+    choiceA:"Event fired when key is pressed";
+    choiceB:"Event fired when key is released";
+    choiceC:"Event fired when a key that procused a character value is presed down";
+    correctanswer: "A"};
+
+
+    question:
+    choiceA:
+    choiceB:
+    choiceC:
+    choiceD:
+
+
+    question:
+    choiceA:
+    choiceB:
+    choiceC:
+    choiceD:
+
+
+
+    question:
+    choiceA:
+    choiceB:
+    choiceC:
+    choiceD:
+
+
+
+    question:
+    choiceA:
+    choiceB:
+    choiceC:
+    choiceD:
+
+
+
+    question:
+    choiceA:
+    choiceB:
+    choiceC:
+    choiceD:
+
+
+    question:
+    choiceA:
+    choiceB:
+    choiceC:
+    choiceD:
+
+
+];
