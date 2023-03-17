3dfeee06e2a5705e7c745ff91745ab5f3768c33f
unknown <harriett.steed@gmail.com>
2023-03-17 01:20:19 -0600

    added questions to JS

--- Assets/script.js
+++ Assets/script.js
@@ -4,58 +4,45 @@ var buttonA = document.getElementById("")
 
 
 
-
+// Quiz questions
 var Questions = [{
-    question:"Keydown is when...?";
-    choiceA:"Event fired when key is pressed";
-    choiceB:"Event fired when key is released";
-    choiceC:"Event fired when a key that procused a character value is presed down";
-    correctanswer: "A"};
+    question:"Keydown is when...?",
+    choiceA:"Event fired when key is pressed",
+    choiceB:"Event fired when key is released",
+    choiceC:"Event fired when a key that procused a character value is presed down",
+    correctAnswer: "A" },
+{
+    question: "Which of these options is NOT a data type?",
+    choiceA: "Null",
+    choiceB:"Boolen",
+    choiceC: "Array",
+    choiceD:"Number",
+    correctAnswer: "C" },
+{
+    question: "Is JavaScript a case-sensitve language?",
+    choiceA: "True",
+    choiceB: "False",
+    correctAnswer: "True" },
+{
+    question:"What type of storage has NO expiration time?",
+    choiceA: "Local storage",
+    choiceB: "Session storage",
+    choiceC: "All of the above!", 
+    correctAnswer: "A" },
 
+{
+    question:"How can you empty an Array in JavaScript?",
+    choiceA:"arrayList = []",
+    choiceB:"arrayList.length = 0;",
+    choiceC:"arrayList.splice(0, arrayList.length);",
+    choiceD: "All of the above",
+    correctAnswer: "D" },
 
-    question:
-    choiceA:
-    choiceB:
-    choiceC:
-    choiceD:
-
-
-    question:
-    choiceA:
-    choiceB:
-    choiceC:
-    choiceD:
-
-
-
-    question:
-    choiceA:
-    choiceB:
-    choiceC:
-    choiceD:
-
-
-
-    question:
-    choiceA:
-    choiceB:
-    choiceC:
-    choiceD:
-
-
-
-    question:
-    choiceA:
-    choiceB:
-    choiceC:
-    choiceD:
-
-
-    question:
-    choiceA:
-    choiceB:
-    choiceC:
-    choiceD:
-
-
+{
+    question:"What logical compariosn operative means equal to?",
+    choiceA:"!==",
+    choiceB:"=",
+    choiceC:"==",
+    choiceD:"===",
+    correctAnswer: "C" },
 ];
