6e28aec0d4005d5f5d4c10b799639d0883d65856
unknown <harriett.steed@gmail.com>
2023-03-22 20:23:19 -0600

    finished HTML, started added var to js, styled the buttons in css

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,15 @@
+// elements from the HTML
+var buttonA = document.getElementId ('a');
+var buttonB = document.getElementById('b');
+var buttonC = document.getElementById('c');
+var buttonD = document.getElementById('d');
+var startButton = document.getElementById('startbtn');
+var finalScoreE1 = document.getElementById("finalScore");
+var quizTimer = document.getElementById("timer");
 
-var buttonA = document.getElementById("")
 
-
-
+// other variables
+var currentQuestion = quizQuestion[currentQuestionIndex];
 
 // Quiz questions
 var Questions = [{
@@ -10,25 +17,25 @@ var Questions = [{
     choiceA:"Event fired when key is pressed",
     choiceB:"Event fired when key is released",
     choiceC:"Event fired when a key that procused a character value is presed down",
-    correctAnswer: "A" },
+    correctAnswer: "a" },
 {
     question: "Which of these options is NOT a data type?",
     choiceA: "Null",
     choiceB:"Boolen",
     choiceC: "Array",
     choiceD:"Number",
-    correctAnswer: "C" },
+    correctAnswer: "c" },
 {
     question: "Is JavaScript a case-sensitve language?",
     choiceA: "True",
     choiceB: "False",
-    correctAnswer: "True" },
+    correctAnswer: "a" },
 {
     question:"What type of storage has NO expiration time?",
     choiceA: "Local storage",
     choiceB: "Session storage",
     choiceC: "All of the above!", 
-    correctAnswer: "A" },
+    correctAnswer: "a" },
 
 {
     question:"How can you empty an Array in JavaScript?",
@@ -36,7 +43,7 @@ var Questions = [{
     choiceB:"arrayList.length = 0;",
     choiceC:"arrayList.splice(0, arrayList.length);",
     choiceD: "All of the above",
-    correctAnswer: "D" },
+    correctAnswer: "d" },
 
 {
     question:"What logical compariosn operative means equal to?",
@@ -44,5 +51,16 @@ var Questions = [{
     choiceB:"=",
     choiceC:"==",
     choiceD:"===",
-    correctAnswer: "C" },
+    correctAnswer: "c" },
 ];
+
+// Function goes through the array containing the quiz questions to generate questions and the answers/
+    function generateQuizQuestion(){
+        gameoverDiv.style.display = "none";
+        if (currentQuestionIndex === finalQuestionIndex){
+            return showScore();
+        }
+        
+    }
+
+
