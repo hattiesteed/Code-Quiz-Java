dc607a604cbf10f0ae7acfc9dfaada200a21b5c5
unknown <harriett.steed@gmail.com>
2023-06-09 00:00:10 -0600

    fixed the tabbing, edited var id

--- Assets/script.js
+++ Assets/script.js
@@ -20,8 +20,6 @@ var endGameBtn = document.getElementById("endGameBtns");
 var submitScoreBtn = document.getElementById("submit");
 
 
-startButton.addEventListener("click", startQuiz);
-
 // Quiz questions
 var Questions = [{
     question:"Keydown is when...?",
@@ -71,54 +69,48 @@ var timerInterval;
 var score = 0;
 var timeLeft = 100;
 var correct;
-
 // Function goes through the array containing the quiz questions to generate questions and the answers/
-    function generateQuizQuestion(){
-        gameOver.style.display = "none";
+function generateQuizQuestion(){
+    gameOver.style.display = "none";
         if (currentQuestionIndex === finalQuestionIndex){
-            return showScore();
+        return showScore();
         }
-        var currentQuestion = quizQuestion[currentQuestionIndex];
-        questionsE1.innerHTML = "<p>" + currentQuestion.question + "</p>";
-        buttonA.innerHTML = currentQuestion.choiceA;
-        buttonB.innerHTML = currentQuestion.choiceB;
-        buttonC.innerHTML = currentQuestion.choiceC;
-        buttonD.innterHTML = currentQuestion.choiceD;
+var currentQuestion = quizQuestion[currentQuestionIndex];
+    questionsE1.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;
+    buttonD.innterHTML = currentQuestion.choiceD;
     };
-
-
-
 // Start Quiz function starts the time, hides the start button, shows the first question
-    function startQuiz(){
-        gameOver.style.display = "none";
-        startQuizDiv.style.display = "none";
-        generateQuizQuestion();
+function startQuiz() {
+    gameOver.style.display = "none";
+    homePage.style.display = "none";
+    generateQuizQuestion();
 
-        // the timer
-        timerInteral = setInterval(function() { 
-            timeLeft--;
-            quizTimer.textContent = "Time left: " + timeLeft;
-
-            if(timeLeft === 0) {
-                clearInterval(timerInterval);
-                showScore();
-        }
+//  timer
+timerInterval = setInterval(function() { 
+timeLeft--;
+quizTimer.textContent = "Time left: " + timeLeft;
+    if(timeLeft === 0) {
+    clearInterval(timerInterval);
+    showScore();
+    }
     }, 1000);
-    quizBody.style.display = "block";
+    quizPage.style.display = "block";
 }
 // end page displays score after finishing the quiz or if u ran out of time
 function showScore(){
-    quizBody.style.display = "none"
+    quizPage.style.display = "none"
     gameOver.style.display = "flex";
     clearInterval (timerInterval);
     highscoreInputName.value = "";
-    finalScoreEl.innerHTML = "Your score " + score + " out of " + quizQuestion.length + "correct";
+    finalScore.innerHTML = "Your score " + score + " out of " + quizQuestion.length + "correct";
 }
 
 // Run function highscore that saves the array of high scores and pushes new username and score into the array on local storage.
 submitScoreBtn.addEventListener("click", function highscore(){
-
-    if(highscoreInputName.value === ""){
+    if(highscoreInputName.value === "") {
         alert("Initials cannot be blank");
         return false; }
     else{
@@ -128,11 +120,13 @@ submitScoreBtn.addEventListener("click", function highscore(){
             name : currentUser,
             score : score
         };
+
         gameOver.style.display = "none";
         highscoreContainer.style.display = "flex";
-        highscore.style.display = "block";
+        savedHighscores.style.display = "block";
         endGameBtn.style.display = "flex";
-            savedHighscores.push(currentHighscore);
+
+        savedHighscores.push(curentHighscore);
         localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
         generateHighscores();
     }
@@ -141,9 +135,9 @@ submitScoreBtn.addEventListener("click", function highscore(){
 // this will clear the list for the high scores and make anew high score list from the local storage
 function generateHighscores(){
     highscoreDisplayName.innerHTML = "";
-    highscoreDisplayName.inert = "";
+    highscoreDisplayScore.innterHTML = "";
     var highscore = JSON.parse(localStorage.getItem("savedHighsocre")) || [];
-    for (i=0; i<highscoreDisplayName.length; i++){
+    for (i=0; i<highscore.length; i++){
         var newName = document.createElement("li");
         var newScore = document.createElement("li");
         newName.textContent = highscores[i].name;
@@ -154,7 +148,7 @@ function generateHighscores(){
 }
 // displays the high scores page while hiding all pages
 function showHighscore(){
-    startQuiz.style.display = "none"
+    homePage.style.display = "none"
     highscoreContainer.style.display = "flex";
     gameOver.style.display = "none";
     highscore.style.display = "block";
@@ -194,4 +188,4 @@ function checkAnswer(answer){
         showScore();
     }
     }
-
+startButton.addEventListener("click", startQuiz);
